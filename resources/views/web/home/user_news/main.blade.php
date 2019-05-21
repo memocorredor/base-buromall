8f67e3e8766375f6c20e03d72f0b02894d2a4ba8
memocorredor <guillermo.corredor@gmail.com>
2019-05-21 17:18:59 -0500

    change

--- resources/views/web/home/user_news/main.blade.php
+++ resources/views/web/home/user_news/main.blade.php
@@ -35,7 +35,7 @@
                             <span>{{ $row_items->created_at }}</span>
                         </div>
                     </div>
-                    <img class="card-img img-fluid" src="{{ Buromall\Http\Controllers\AdminUserNewController::getImagen(3)->toJson() }}" alt="Card image cap">
+                    <img class="card-img img-fluid" src="{{ Buromall\Http\Controllers\AdminUserNewController::getImagen(3) }}" alt="Card image cap">
                     <div class="card-stats">
                         <div class="row no-gutters">
                             <div class="col-4 d-flex justify-content-center border-stat">

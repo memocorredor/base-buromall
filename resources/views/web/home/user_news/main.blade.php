51bd7c750579e5553bbc60fb1f1ba4b9b8ad201b
memocorredor <guillermo.corredor@gmail.com>
2019-05-21 17:49:12 -0500

    cambio

--- resources/views/web/home/user_news/main.blade.php
+++ resources/views/web/home/user_news/main.blade.php
@@ -35,7 +35,7 @@
                             <span>{{ $row_items->created_at }}</span>
                         </div>
                     </div>
-                    <img class="card-img img-fluid" src="{{ Buromall\AppCore\imageNews(3) }}" alt="Card image cap">
+                    <img class="card-img img-fluid" src="{{ Buromall\AppCore\coreImage::imageNews(3) }}" alt="Card image cap">
                     <div class="card-stats">
                         <div class="row no-gutters">
                             <div class="col-4 d-flex justify-content-center border-stat">

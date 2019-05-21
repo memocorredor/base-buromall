b2a064db003f383d9008bcac0913ae300ce3a1da
memocorredor <guillermo.corredor@gmail.com>
2019-05-21 08:37:11 -0500

    Cambio en map test

--- resources/views/web/home/contact/main.blade.php
+++ resources/views/web/home/contact/main.blade.php
@@ -1,11 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<div class="container py-4">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div id="map"></div>
+<div id="map"></div>
 
-        </div>
-    </div>
-</div>
 @endsection

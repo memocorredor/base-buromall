fce2860b06637077cafa1b560f757038cb623756
memocorredor <guillermo.corredor@gmail.com>
2019-05-21 19:29:50 -0500

    Cambios

--- resources/views/components/home/item_comments.blade.php
+++ resources/views/components/home/item_comments.blade.php
@@ -1,1 +1,12 @@
-comentarios
+<div class="container py-4">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">En mantenimiento!</div>
+                    <div class="card-body">
+                        <h6 style="width: 100%; text-align: center;">Pronto tendremos habilitado el sistema de comentarios!</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>

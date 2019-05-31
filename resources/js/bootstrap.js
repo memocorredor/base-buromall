2fc7c8edf87d03048ca84aed825571d2cefc4f7e
memocorredor <guillermo.corredor@gmail.com>
2019-05-31 11:09:03 -0500

    cambios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -88,9 +88,8 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: process.env.MIX_PUSHER_APP_PORT,
-    wssPort: process.env.MIX_PUSHER_APP_PORT,
     disableStats: process.env.MIX_PUSHER_APP_STATSH,
-    enabledTransports: ['ws', 'wss'],
+    encrypted: process.env.PUSHER_APP_ENCRYPT,
 });
 
 
